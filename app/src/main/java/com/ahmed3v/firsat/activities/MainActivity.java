adb72e499801e0b733a40c35cbb774a7e5f4dd82
iAhmed3v <devahmdali@gmail.com>
2021-06-22 15:39:31 +0300

    Updating the Cars ArrayList

--- app/src/main/java/com/ahmed3v/firsat/activities/MainActivity.java
+++ app/src/main/java/com/ahmed3v/firsat/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ahmed3v.firsat;
+package com.ahmed3v.firsat.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -6,23 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ahmed3v.firsat.R;
 import com.ahmed3v.firsat.adapter.CarAdapter;
 import com.ahmed3v.firsat.model.Car;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -125,11 +122,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         ArrayList<Car> list = new ArrayList<>();
 
-        list.add(new Car("https://i0.shbdn.com/photos/41/47/97/x5_863414797l7w.jpg" ,"Fiat - Egea 2017-2018", "السعر اليومي: 250 TL", "السعر الشهري: 4500 TL"));
-        list.add(new Car("https://besthqwallpapers.com/Uploads/18-8-2019/101678/thumb2-fiat-egea-sport-4k-2019-cars-motion-blur-fiat-357.jpg" ,"Fiat - Egea 2019", "السعر اليومي: 300 TL", "السعر الشهري: 5000 TL"));
-        list.add(new Car( "https://i0.shbdn.com/photos/96/19/94/x5_89396199418l.jpg","Renault - Clio 2018", "السعر اليومي: 250 TL", "السعر الشهري: 4500 TL"));
-        list.add(new Car("http://www.arabateknikbilgi.com/wp-content/uploads/2018/01/ArabaTeknikBilgi-2018-Hyundai-Elantra.jpg" ,"Hyundai - Elantra 2018", "السعر اليومي: 300 TL", "السعر الشهري: 6000 TL"));
-        list.add(new Car("https://media.autoexpress.co.uk/image/private/s--DLhebqK4--/v1562243256/autoexpress/2016/08/uk-grand-c-max-front-three-quart-dynamic.jpg" ,"Ford - C-Max 2017", "السعر اليومي: 400 TL", "السعر الشهري: 10000 TL"));
+        list.add(new Car("https://i0.shbdn.com/photos/41/47/97/x5_863414797l7w.jpg" ,"Fiat - Egea 2017-2018", "السعر اليومي: 300 TL", "السعر الشهري: 5000 TL"));
+        list.add(new Car("https://besthqwallpapers.com/Uploads/18-8-2019/101678/thumb2-fiat-egea-sport-4k-2019-cars-motion-blur-fiat-357.jpg" ,"Fiat - Egea 2019", "السعر اليومي: 350 TL", "السعر الشهري: 5500 TL"));
+        list.add(new Car( "https://i0.shbdn.com/photos/96/19/94/x5_89396199418l.jpg","Renault - Clio 2018", "السعر اليومي: 300 TL", "السعر الشهري: 5000 TL"));
+        list.add(new Car("http://www.arabateknikbilgi.com/wp-content/uploads/2018/01/ArabaTeknikBilgi-2018-Hyundai-Elantra.jpg" ,"Hyundai - Elantra 2018", "السعر اليومي: 350 TL", "السعر الشهري: 6000 TL"));
+        list.add(new Car("https://media.autoexpress.co.uk/image/private/s--DLhebqK4--/v1562243256/autoexpress/2016/08/uk-grand-c-max-front-three-quart-dynamic.jpg" ,"Ford - C-Max 2017", "السعر اليومي: 450 TL", "السعر الشهري: 10000 TL"));
 
         return list;
     }
